ac5186067ab4fdcdf0e90ef7a8d8cf05917b3aaf
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2024-04-05 00:21:16 +0600

    address sender moved to env

--- config/abi-config.js
+++ config/abi-config.js
@@ -1,7 +1,7 @@
 export const CHAINID = [42220];
 export const CHAINID_HEX = '0xa4ec'
 export const RPC = 'https://forno.celo.org';
-export const ADDRESS_SENDER = "0x5f0366c9962193fA774cdce9602195593B49f23C";
+export const ADDRESS_SENDER = process.env.NEXT_PUBLIC_ADDRESS_SENDER;
 
 export const CHARGE_VALUE = 1; //1 Celo
 export const CHARGE_PER = 200; //array of 200 address in 1 tx
