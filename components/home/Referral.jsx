3ff5ea4233b07247ebe5870a0b95ce4313c664e2
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2024-05-18 01:06:30 +0600

    referral system

--- components/home/Referral.jsx
+++ components/home/Referral.jsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
 import { uiMetaData } from "../../data";
+import { useState } from "react";
 
-const {name, siteName, title} = uiMetaData;
+const { name, siteName, title } = uiMetaData;
 
 const Referral = () => {
+  const [value, setValue] = useState("");
+  const [referalLink, setReferalLink] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const generateReff = () => {
+    if (value) {
+      setErrorMsg('');
+      const refLink = `${process.env.NEXT_PUBLIC_LIVE_URL}${value.toLowerCase()}`
+      setReferalLink(refLink)
+    } else {
+      return setErrorMsg('Past your wallet id first')
+    }
+  }
   return (
     <div className="bg-[#1A1A1A]">
       <div className="max-w-4xl m-auto px-3 py-6 md:pl-0" data-aos="fade-up">
@@ -13,8 +28,7 @@ const Referral = () => {
           Earn {title} by referring people to {siteName}
         </p>
 
-        <h1 className="text-fb font-extrabold text-4xl" data-aos="zoom-in">{name} Referral System Coming Soon</h1>
-        {/* <p className="text-sm py-1">
+        <p className="text-sm py-1">
           Insert the {title} address you would like to receive your {title}
           tokens, and generate your unique referral address.
         </p>
@@ -23,15 +37,22 @@ const Referral = () => {
           your unique referral link.
         </p>
 
-        <button className="bg-fb text-black px-4 py-2 rounded-md font-semibold my-3">
+        {!referalLink ? <><button className="bg-fb text-black px-4 py-2 rounded-md font-semibold my-3" onClick={() => generateReff()}>
           Generate referral link
         </button>
-        <br />
-        <input
-          type="text"
-          placeholder={`Input ${title} address`}
-          className="bg-black text-white px-4 py-3 rounded-md my-2 md:w-[300px] placeholder-fb"
-        /> */}
+          <br />
+          <input
+            type="text"
+            placeholder={`Input ${title} address`}
+            className="bg-black text-white px-4 py-3 rounded-md my-2 md:w-[300px] placeholder-fb"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          {errorMsg && <p className="text-red-700 font-bold mt-2 ml-1">{errorMsg}</p>}
+        </> : <div>
+          <button className="bg-fb text-black font-semibold rounded-md py-2 px-4 my-3 flex gap-2 items-center" data-aos="fade-up" onClick={() => window.open(referalLink, '_blank')}>
+            Share referral link <Image src="/images/send.png" alt="" width={18} height={18} />
+          </button>
+        </div>}
       </div>
     </div>
   );
